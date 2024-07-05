249ac6879302f70f443c2d8950baf1319797719e
Yashodeep Joshi <96687722+Yashodeepj7@users.noreply.github.com>
2024-07-05 23:36:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { backgroundImage: {
+      'custom-gradient': 'linear-gradient(to right, #ffffff 50%, #ffc0cb 50%)',
+    }},
+  },
+  plugins: [],
+}
+
